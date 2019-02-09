1d8842cd3ce1d4e53c20479c05be9f8d75bffbd5
Guru <gurpreet.usit@gmail.com>
2019-02-09 20:46:08 +0800

    getting ready for custom model

--- app/src/main/java/com/guru/mlkitdemofirebase/ui/CustomModelsFragment.kt
+++ app/src/main/java/com/guru/mlkitdemofirebase/ui/CustomModelsFragment.kt
@@ -7,7 +7,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.ml.custom.FirebaseModelInterpreter
 import com.google.firebase.ml.custom.FirebaseModelManager
+import com.google.firebase.ml.custom.FirebaseModelOptions
 import com.google.firebase.ml.custom.model.FirebaseCloudModelSource
 import com.google.firebase.ml.custom.model.FirebaseModelDownloadConditions
 
@@ -35,12 +37,28 @@ class CustomModelsFragment : Fragment() {
 
 // Build a FirebaseCloudModelSource object by specifying the name you assigned the model
 // when you uploaded it in the Firebase console.
-        val cloudSource = FirebaseCloudModelSource.Builder("my_cloud_model")
+        val cloudSource = FirebaseCloudModelSource.Builder("mobilenet_v1")
                 .enableModelUpdates(true)
                 .setInitialDownloadConditions(conditions)
                 .setUpdatesDownloadConditions(conditions)
                 .build()
         FirebaseModelManager.getInstance().registerCloudModelSource(cloudSource)
+
+        val options = FirebaseModelOptions.Builder()
+                .setCloudModelName("mobilenet_v1")
+                .build()
+        val interpreter = FirebaseModelInterpreter.getInstance(options)
+
+        interpre = tf.lite.Interpreter(model_path="my_model.tflite")
+        interpreter.allocate_tensors()
+
+        # Print input shape and type
+        print(interpreter.get_input_details()[0]['shape'])  # Example: [1 224 224 3]
+        print(interpreter.get_input_details()[0]['dtype'])  # Example: <class 'numpy.float32'>
+
+        # Print output shape and type
+        print(interpreter.get_output_details()[0]['shape'])  # Example: [1 1000]
+        print(interpreter.get_output_details()[0]['dtype'])
 
     }
 
